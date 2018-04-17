15f45479af1ced5328bf62e66319a3038fdc68eb
lutfiqaraman <lutfiqaraman@gmail.com>
2018-04-17 21:32:16 +0300

    Done

--- MoneyPicker/Program.cs
+++ MoneyPicker/Program.cs
@@ -10,6 +10,14 @@ namespace MoneyPicker
     {
         static void Main(string[] args)
         {
+            var amount   = (decimal) Math.Round(235.00345, 4);
+            string coins = (amount % Decimal.One).ToString().Substring(2);
+
+            Console.WriteLine("Amount : {0}", amount);
+            Console.WriteLine("Papers : {0}", (int) amount);
+            Console.WriteLine("Coins  : {0}", coins);
+
+            Console.ReadKey();
         }
     }
 }
